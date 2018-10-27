03937697693aac841e193a35b81efb2343e647d7
Ville Brofeldt <ville.brofeldt@iki.fi>
2018-10-27 10:13:01 +0300

    Add base modules

---
+++ pyhtzee/classes.py
@@ -0,0 +1,22 @@
+"""
+Common classes needed for gameplay
+"""
+from enum import IntEnum
+
+
+class Category(IntEnum):
+    ACES = 0
+    TWOS = 1
+    THREES = 2
+    FOURS = 3
+    FIVES = 4
+    SIXES = 5
+    THREE_OF_A_KIND = 6
+    FOUR_OF_A_KIND = 7
+    FULL_HOUSE = 8
+    SMALL_STRAIGHT = 9
+    LARGE_STRAIGHT = 10
+    YAHTZEE = 11
+    CHANCE = 12
+    UPPER_SECTION_BONUS = 13
+    EXTRA_YAHTZEES = 14
