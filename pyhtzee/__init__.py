14d56cd9ebbc93be49c4059deba6a832e363db18
Ville Brofeldt <ville.brofeldt@iki.fi>
2018-10-29 22:21:12 +0200

    Fix error in non-joker rules and add unit test

---
+++ pyhtzee/__init__.py
@@ -0,0 +1,128 @@
+from random import Random
+from typing import Dict
+
+from pyhtzee.classes import Category, Rule
+from pyhtzee.utils import (
+    action_to_category_map,
+    action_to_dice_roll_map,
+    CATEGORY_ACTION_OFFSET,
+    category_to_action_map,
+    category_to_scoring_function_map,
+    is_joker_category,
+    is_upper_section_category,
+)
+from pyhtzee.scoring import CONSTANT_SCORES, score_upper_section_bonus
+
+
+class Pyhtzee:
+    def __init__(self, seed: int = None, rule: Rule = None):
+        self.scores: Dict[Category, int] = {}
+        self.rule = rule if rule else Rule.FREE_CHOICE_JOKER
+
+        # a game has a total of 12 rounds
+        self.round = 0
+
+        # a round consists of max 3 dice rolls + 1 action = 4. Sub-round zero is
+        # reserved for two player game when a player has not yet started the round
+        self.sub_round = 1
+
+        if seed:
+            self.rnd = Random(seed)
+        else:
+            self.rnd = Random()
+
+        # initialize dice
+        self.dice = [0, 0, 0, 0, 0]
+        self.roll_dice(True, True, True, True, True)
+
+    def roll_dice(self, d1: bool, d2: bool, d3: bool, d4: bool, d5: bool):
+        dice = [d1, d2, d3, d4, d5]
+        for i, die in enumerate(dice):
+            if die:
+                self.dice[i] = self.rnd.choice([1, 2, 3, 4, 5, 6])
+
+    def get_possible_actions(self):
+        possible_actions = []
+
+        # determine if rerolling dice is possible; is so, add all possible permutations
+        if self.sub_round < 3:
+            possible_actions.extend(list(range(CATEGORY_ACTION_OFFSET)))
+
+        # See which categories are still unused
+        for category in Category:
+            if not self.scores.get(category):
+                action = category_to_action_map.get(category)
+                # Check if the category has an action associated with it
+                # (upper section bonus is automatic).
+                if action:
+                    possible_actions.append(action)
+
+        return possible_actions
+
+    def sample_action(self):
+        actions = self.get_possible_actions()
+        return self.rnd.sample(actions, 1)[0]
+
+    def take_action(self, action: int) -> int:
+        possible_actions = self.get_possible_actions()
+        if action not in possible_actions:
+            return 0
+
+        # if dice rolling action
+        if action < CATEGORY_ACTION_OFFSET:
+            self.sub_round += 1
+            self.roll_dice(*action_to_dice_roll_map[action])
+            return 0
+
+        # all non-rolling actions lead to the sub-round
+        # ending and moving to the next round
+        self.round += 1
+        self.sub_round = 0
+
+        scores = self.get_action_score(action)
+        for k, v in scores.items():
+            old_score = self.scores.get(k, 0)
+            self.scores[k] = old_score + v
+        return sum(scores.values())
+
+    def is_eligible_for_yahtzee_bonus(self):
+        if self.is_yahtzee() and self.scores.get(Category.YAHTZEE, 0) > 0:
+            return True
+        return False
+
+    def is_yahtzee(self):
+        return True if len(set(self.dice)) == 1 else False
+
+    def is_finished(self):
+        return True if self.round == 13 else False
+
+    def get_total_score(self):
+        return sum([v for v in self.scores.values()])
+
+    def get_action_score(self, action: int) -> Dict[Category, int]:
+        category = action_to_category_map[action]
+        scores: Dict[Category, int] = {}
+
+        # yahtzee bonus
+        if self.is_eligible_for_yahtzee_bonus():
+            scores[Category.YAHTZEE_BONUS] = CONSTANT_SCORES[Category.YAHTZEE_BONUS]
+
+        # Joker rule
+        if self.is_eligible_for_yahtzee_bonus() and \
+                self.rule == Rule.FREE_CHOICE_JOKER and \
+                is_joker_category(category):
+            scores[category] = CONSTANT_SCORES[category]
+        else:  # Regular rule
+            scoring_function = category_to_scoring_function_map[category]
+            scores[category] = scoring_function(self.dice)
+
+        # upper section bonus
+        if is_upper_section_category(category):
+            upper_scores = [v for k, v in self.scores.items()
+                            if int(k) <= int(Category.SIXES)]
+            upper_scores.append(category)
+            if len(upper_scores) == 6:
+                bonus_reward = score_upper_section_bonus(sum(upper_scores))
+                scores[Category.UPPER_SECTION_BONUS] = bonus_reward
+
+        return scores
--- tests/pyhtzee_tests.py
+++ tests/pyhtzee_tests.py
@@ -1,6 +1,5 @@
-from pyhtzee.classes import Category
-from pyhtzee.state import State
-from pyhtzee import utils
+from pyhtzee.classes import Category, Rule
+from pyhtzee import Pyhtzee, utils
 from pyhtzee.utils import (
     dice_roll_to_action_map,
     category_to_action_map
@@ -9,114 +8,145 @@ from pyhtzee.utils import (
 from unittest import TestCase
 
 
-class StateTestCase(TestCase):
+class PyhtzeeTestCase(TestCase):
     def test_dice_values(self):
-        state = State()
-        for die in state.dice:
+        pyhtzee = Pyhtzee()
+        for die in pyhtzee.dice:
             self.assertTrue(1 <= die <= 6)
 
     def test_rolling_of_dice(self):
-        state = State(seed=123)
-        self.assertListEqual([1, 3, 1, 4, 3], state.dice)
+        pyhtzee = Pyhtzee(seed=123)
+        self.assertListEqual([1, 3, 1, 4, 3], pyhtzee.dice)
         action = utils.dice_roll_to_action_map[(True, True, True, True, True)]
-        reward = state.take_action(action)
-        self.assertListEqual([1, 1, 4, 5, 5], state.dice)
+        reward = pyhtzee.take_action(action)
+        self.assertListEqual([1, 1, 4, 5, 5], pyhtzee.dice)
         self.assertEqual(reward, 0)
         action = utils.dice_roll_to_action_map[(False, False, False, False, True)]
-        state.take_action(action)
-        self.assertListEqual([1, 1, 4, 5, 3], state.dice)
+        pyhtzee.take_action(action)
+        self.assertListEqual([1, 1, 4, 5, 3], pyhtzee.dice)
 
     def test_full_round_four_threes(self):
-        state = State(seed=234)
-        self.assertListEqual([3, 3, 1, 5, 4], state.dice)
+        pyhtzee = Pyhtzee(seed=234)
+        self.assertListEqual([3, 3, 1, 5, 4], pyhtzee.dice)
         action = utils.dice_roll_to_action_map[(False, False, True, True, True)]
-        state.take_action(action)
-        self.assertListEqual([3, 3, 5, 6, 4], state.dice)
+        pyhtzee.take_action(action)
+        self.assertListEqual([3, 3, 5, 6, 4], pyhtzee.dice)
         action = utils.dice_roll_to_action_map[(False, False, True, True, True)]
-        state.take_action(action)
-        self.assertListEqual([3, 3, 2, 3, 3], state.dice)
+        pyhtzee.take_action(action)
+        self.assertListEqual([3, 3, 2, 3, 3], pyhtzee.dice)
         action = category_to_action_map[Category.THREES]
-        reward = state.take_action(action)
+        reward = pyhtzee.take_action(action)
         self.assertEqual(reward, 12)
-        self.assertEqual(state.scores[Category.THREES], reward)
+        self.assertEqual(pyhtzee.scores[Category.THREES], reward)
 
     def test_full_round_unsuccessful_yahtzee(self):
-        state = State(seed=234)
-        self.assertListEqual([3, 3, 1, 5, 4], state.dice)
+        pyhtzee = Pyhtzee(seed=234)
+        self.assertListEqual([3, 3, 1, 5, 4], pyhtzee.dice)
         action = category_to_action_map[Category.YAHTZEE]
-        reward = state.take_action(action)
+        reward = pyhtzee.take_action(action)
         self.assertEqual(reward, 0)
-        self.assertEqual(state.scores[Category.YAHTZEE], reward)
+        self.assertEqual(pyhtzee.scores[Category.YAHTZEE], reward)
 
     def test_completing_roung(self):
-        state = State(seed=345)
+        pyhtzee = Pyhtzee(seed=345)
         action = dice_roll_to_action_map[(True, True, True, True, True)]
-        state.take_action(action)
-        state.take_action(action)
-        self.assertListEqual([3, 4, 6, 3, 1], state.dice)
+        pyhtzee.take_action(action)
+        pyhtzee.take_action(action)
+        self.assertListEqual([3, 4, 6, 3, 1], pyhtzee.dice)
 
     def test_full_round_successful_yahtzee(self):
-        state = State()
-        state.dice = [1, 1, 1, 1, 1]
+        pyhtzee = Pyhtzee()
+        pyhtzee.dice = [1, 1, 1, 1, 1]
         action = category_to_action_map[Category.YAHTZEE]
-        reward = state.take_action(action)
+        reward = pyhtzee.take_action(action)
         self.assertEqual(reward, 50)
-        self.assertEqual(state.scores[Category.YAHTZEE], reward)
+        self.assertEqual(pyhtzee.scores[Category.YAHTZEE], reward)
 
     def test_full_round_successful_chance(self):
-        state = State()
-        state.dice = [6, 6, 6, 6, 5]
+        pyhtzee = Pyhtzee()
+        pyhtzee.dice = [6, 6, 6, 6, 5]
         action = category_to_action_map[Category.CHANCE]
-        reward = state.take_action(action)
+        reward = pyhtzee.take_action(action)
         self.assertEqual(reward, 29)
-        self.assertEqual(state.scores[Category.CHANCE], reward)
+        self.assertEqual(pyhtzee.scores[Category.CHANCE], reward)
 
     def test_upper_section_bonus(self):
-        state = State()
-        state.dice = [1, 1, 1, 1, 1]
-        state.take_action(category_to_action_map[Category.ACES])
-        state.dice = [2, 2, 2, 2, 2]
-        state.take_action(category_to_action_map[Category.TWOS])
-        state.dice = [3, 3, 3, 3, 3]
-        state.take_action(category_to_action_map[Category.THREES])
-        state.dice = [4, 4, 4, 4, 4]
-        state.take_action(category_to_action_map[Category.FOURS])
-        state.dice = [5, 5, 5, 5, 5]
-        state.take_action(category_to_action_map[Category.FIVES])
-        state.dice = [6, 6, 6, 6, 6]
+        pyhtzee = Pyhtzee()
+        pyhtzee.dice = [1, 1, 1, 1, 1]
+        pyhtzee.take_action(category_to_action_map[Category.ACES])
+        pyhtzee.dice = [2, 2, 2, 2, 2]
+        pyhtzee.take_action(category_to_action_map[Category.TWOS])
+        pyhtzee.dice = [3, 3, 3, 3, 3]
+        pyhtzee.take_action(category_to_action_map[Category.THREES])
+        pyhtzee.dice = [4, 4, 4, 4, 4]
+        pyhtzee.take_action(category_to_action_map[Category.FOURS])
+        pyhtzee.dice = [5, 5, 5, 5, 5]
+        pyhtzee.take_action(category_to_action_map[Category.FIVES])
+        pyhtzee.dice = [6, 6, 6, 6, 6]
         action = category_to_action_map[Category.SIXES]
-        reward = state.take_action(action)
+        reward = pyhtzee.take_action(action)
         self.assertEqual(reward, 65)
-        self.assertEqual(state.scores[Category.SIXES], 30)
-        self.assertEqual(state.scores[Category.UPPER_SECTION_BONUS], 35)
+        self.assertEqual(pyhtzee.scores[Category.SIXES], 30)
+        self.assertEqual(pyhtzee.scores[Category.UPPER_SECTION_BONUS], 35)
 
-    def test_perfect_score(self):
-        state = State()
-        state.dice = [6, 6, 6, 6, 6]
-        state.take_action(category_to_action_map[Category.YAHTZEE])
-        state.dice = [1, 1, 1, 1, 1]
-        state.take_action(category_to_action_map[Category.ACES])
-        state.dice = [2, 2, 2, 2, 2]
-        state.take_action(category_to_action_map[Category.TWOS])
-        state.dice = [3, 3, 3, 3, 3]
-        state.take_action(category_to_action_map[Category.THREES])
-        state.dice = [4, 4, 4, 4, 4]
-        state.take_action(category_to_action_map[Category.FOURS])
-        state.dice = [5, 5, 5, 5, 5]
-        state.take_action(category_to_action_map[Category.FIVES])
-        state.dice = [6, 6, 6, 6, 6]
-        state.take_action(category_to_action_map[Category.SIXES])
-        state.dice = [6, 6, 6, 6, 6]
-        state.take_action(category_to_action_map[Category.THREE_OF_A_KIND])
-        state.dice = [6, 6, 6, 6, 6]
-        state.take_action(category_to_action_map[Category.FOUR_OF_A_KIND])
-        state.dice = [6, 6, 6, 6, 6]
-        state.take_action(category_to_action_map[Category.FULL_HOUSE])
-        state.dice = [6, 6, 6, 6, 6]
-        state.take_action(category_to_action_map[Category.SMALL_STRAIGHT])
-        state.dice = [6, 6, 6, 6, 6]
-        state.take_action(category_to_action_map[Category.LARGE_STRAIGHT])
-        state.dice = [6, 6, 6, 6, 6]
-        state.take_action(category_to_action_map[Category.CHANCE])
-        self.assertEqual(state.get_total_score(), 1575)
-        self.assertTrue(state.is_finished())
+    def test_perfect_joker_score(self):
+        pyhtzee = Pyhtzee(rule=Rule.FREE_CHOICE_JOKER)
+        pyhtzee.dice = [6, 6, 6, 6, 6]
+        pyhtzee.take_action(category_to_action_map[Category.YAHTZEE])
+        pyhtzee.dice = [1, 1, 1, 1, 1]
+        pyhtzee.take_action(category_to_action_map[Category.ACES])
+        pyhtzee.dice = [2, 2, 2, 2, 2]
+        pyhtzee.take_action(category_to_action_map[Category.TWOS])
+        pyhtzee.dice = [3, 3, 3, 3, 3]
+        pyhtzee.take_action(category_to_action_map[Category.THREES])
+        pyhtzee.dice = [4, 4, 4, 4, 4]
+        pyhtzee.take_action(category_to_action_map[Category.FOURS])
+        pyhtzee.dice = [5, 5, 5, 5, 5]
+        pyhtzee.take_action(category_to_action_map[Category.FIVES])
+        pyhtzee.dice = [6, 6, 6, 6, 6]
+        pyhtzee.take_action(category_to_action_map[Category.SIXES])
+        pyhtzee.dice = [6, 6, 6, 6, 6]
+        pyhtzee.take_action(category_to_action_map[Category.THREE_OF_A_KIND])
+        pyhtzee.dice = [6, 6, 6, 6, 6]
+        pyhtzee.take_action(category_to_action_map[Category.FOUR_OF_A_KIND])
+        pyhtzee.dice = [6, 6, 6, 6, 6]
+        pyhtzee.take_action(category_to_action_map[Category.FULL_HOUSE])
+        pyhtzee.dice = [6, 6, 6, 6, 6]
+        pyhtzee.take_action(category_to_action_map[Category.SMALL_STRAIGHT])
+        pyhtzee.dice = [6, 6, 6, 6, 6]
+        pyhtzee.take_action(category_to_action_map[Category.LARGE_STRAIGHT])
+        pyhtzee.dice = [6, 6, 6, 6, 6]
+        pyhtzee.take_action(category_to_action_map[Category.CHANCE])
+        self.assertEqual(pyhtzee.get_total_score(), 1575)
+        self.assertTrue(pyhtzee.is_finished())
+
+    def test_perfect_regular_score(self):
+        pyhtzee = Pyhtzee(rule=Rule.REGULAR)
+        pyhtzee.dice = [6, 6, 6, 6, 6]
+        pyhtzee.take_action(category_to_action_map[Category.YAHTZEE])
+        pyhtzee.dice = [1, 1, 1, 1, 1]
+        pyhtzee.take_action(category_to_action_map[Category.ACES])
+        pyhtzee.dice = [2, 2, 2, 2, 2]
+        pyhtzee.take_action(category_to_action_map[Category.TWOS])
+        pyhtzee.dice = [3, 3, 3, 3, 3]
+        pyhtzee.take_action(category_to_action_map[Category.THREES])
+        pyhtzee.dice = [4, 4, 4, 4, 4]
+        pyhtzee.take_action(category_to_action_map[Category.FOURS])
+        pyhtzee.dice = [5, 5, 5, 5, 5]
+        pyhtzee.take_action(category_to_action_map[Category.FIVES])
+        pyhtzee.dice = [6, 6, 6, 6, 6]
+        pyhtzee.take_action(category_to_action_map[Category.SIXES])
+        pyhtzee.dice = [6, 6, 6, 6, 6]
+        pyhtzee.take_action(category_to_action_map[Category.THREE_OF_A_KIND])
+        pyhtzee.dice = [6, 6, 6, 6, 6]
+        pyhtzee.take_action(category_to_action_map[Category.FOUR_OF_A_KIND])
+        pyhtzee.dice = [6, 6, 6, 6, 6]
+        pyhtzee.take_action(category_to_action_map[Category.FULL_HOUSE])
+        pyhtzee.dice = [6, 6, 6, 6, 6]
+        pyhtzee.take_action(category_to_action_map[Category.SMALL_STRAIGHT])
+        pyhtzee.dice = [6, 6, 6, 6, 6]
+        pyhtzee.take_action(category_to_action_map[Category.LARGE_STRAIGHT])
+        pyhtzee.dice = [6, 6, 6, 6, 6]
+        pyhtzee.take_action(category_to_action_map[Category.CHANCE])
+        self.assertEqual(pyhtzee.get_total_score(), 1505)
+        self.assertTrue(pyhtzee.is_finished())
